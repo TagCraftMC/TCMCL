a02ef31b1855661e80601e457362391595fcaafd
ammarx <ammar.ahmad1993@gmail.com>
2014-01-20 13:13:33 +0500

    me.hide()

--- Launcher/Form3.vb
+++ Launcher/Form3.vb
@@ -3,4 +3,8 @@
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles Me.Load
         Label2.Text = Form1.result
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+    End Sub
 End Class
