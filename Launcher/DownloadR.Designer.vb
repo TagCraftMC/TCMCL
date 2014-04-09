74748e563e18d44cb70a27003ab2876aa37482e9
Chalkie <Tagcraftmc@gmail.com>
2014-04-09 17:49:15 +1000

    Small changes to the Dependencies Downloader

--- Launcher/DownloadR.Designer.vb
+++ Launcher/DownloadR.Designer.vb
@@ -22,10 +22,12 @@ Partial Class DownloadR
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(DownloadR))
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TextBox1
@@ -33,6 +35,7 @@ Partial Class DownloadR
         Me.TextBox1.Location = New System.Drawing.Point(15, 39)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
         Me.TextBox1.Size = New System.Drawing.Size(388, 118)
         Me.TextBox1.TabIndex = 0
         '
@@ -49,7 +52,7 @@ Partial Class DownloadR
         '
         Me.ProgressBar1.Location = New System.Drawing.Point(15, 163)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(387, 23)
+        Me.ProgressBar1.Size = New System.Drawing.Size(387, 15)
         Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee
         Me.ProgressBar1.TabIndex = 2
         '
@@ -58,16 +61,29 @@ Partial Class DownloadR
         Me.BackgroundWorker1.WorkerReportsProgress = True
         Me.BackgroundWorker1.WorkerSupportsCancellation = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 181)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(366, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Note: Some Mods such as Forge need to download dependencies from their" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "servers, " & _
+    "that is what we are attempting to do now."
+        '
         'DownloadR
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(414, 212)
+        Me.ClientSize = New System.Drawing.Size(414, 232)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "DownloadR"
-        Me.Text = "DownloadR"
+        Me.Text = "Dependencies Downloading..."
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -76,4 +92,5 @@ Partial Class DownloadR
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 End Class
